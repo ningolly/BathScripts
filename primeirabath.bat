dce1195ec6f8def8b38a98f84356caec6bc4db62
ningolly <nicollydev@gmail.com>
2024-06-27 00:11:09 -0300

    Add files via upload

---
+++ primeirabath.bat
@@ -0,0 +1,17 @@
+@echo off
+echo "Bem-vindo! Vamos começar limpando a tela."
+pause
+cls
+echo "A tela foi limpa. Aguarde..."
+pause
+cls
+echo "Preparando para mostrar arquivos..."
+pause
+dir /p
+pause
+echo "A estrutura de pastas será exibida a seguir..."
+pause
+tree /f
+pause
+echo "Processo concluído. Obrigado!"
+pause
